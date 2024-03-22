c1ddbf5277bba8a098c4bcb750029f8d54d16725
wangxuezhu <2565439576@qq.com>
2024-03-22 14:33:11 +0000

    finish my_conv_cuda

--- torchsparse/backend/convolution/my_conv_cuda.cu
+++ torchsparse/backend/convolution/my_conv_cuda.cu
@@ -12,16 +12,16 @@
 
 #define cdiv(x, y) (((x) + (y) - 1) / (y))
 
-__device__ void load_shm_A(half *shm_A, half *A, int* out_in_map, int c_in, int kernel_volume, int ko)
+__device__ void load_shm_A(half *shm_A, half *A, int* reorder_map, int valid, int M, int c_in, int ko)
 {
     // layout: [64, 64]
     int tid = threadIdx.y * 32 + threadIdx.x;
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < valid * 2; i++) {
         int row = i * 8 + tid / 8;
         int col = tid % 8 * 8;
         int shm_row = row;
         int shm_col = col ^ ((shm_row & 3) << 3);
-        int row_A = out_in_map[(blockIdx.x * 64 + row) * kernel_volume + (ko * 64 + col) / c_in];
+        int row_A = reorder_map[row];
         int col_A = (ko * 64 + col) % c_in;
         void* ptr = &shm_A[shm_row * 64 + shm_col];
         if (row_A == -1) {
@@ -84,7 +84,7 @@ __device__ void load_reg_B(uint32_t* reg_B, half* shm_B, int ki)
     }
 }
 
-__device__ void store_reg_C(uint32_t* reg_C, float* shm_C, int mi)
+__device__ void store_reg_C(uint32_t* reg_C, float* shm_C, int* loc_map, int mi)
 {
     int lane_id = threadIdx.x;
 
@@ -92,10 +92,12 @@ __device__ void store_reg_C(uint32_t* reg_C, float* shm_C, int mi)
         int row = mi * 16 + lane_id / 4;
         int col = threadIdx.y * 32 + ni * 8 + (lane_id % 4) * 2;
         col = col ^ ((row & 3) << 3);
-        shm_C[row * 64 + col] += *(float*)(&reg_C[ni * 4]);
-        shm_C[row * 64 + col + 1] += *(float*)(&reg_C[ni * 4 + 1]);
-        shm_C[(row + 8) * 64 + col] += *(float*)(&reg_C[ni * 4 + 2]);
-        shm_C[(row + 8) * 64 + col + 1] += *(float*)(&reg_C[ni * 4 + 3]);
+        if (loc_map[row] != -1) {
+            shm_C[loc_map[row] * 64 + col] += *(float*)(&reg_C[ni * 4]);
+            shm_C[loc_map[row] * 64 + col + 1] += *(float*)(&reg_C[ni * 4 + 1]);
+            shm_C[loc_map[row + 8] * 64 + col] += *(float*)(&reg_C[ni * 4 + 2]);
+            shm_C[loc_map[row + 8] * 64 + col + 1] += *(float*)(&reg_C[ni * 4 + 3]);
+        }
     }
 }
 
@@ -112,8 +114,9 @@ __device__ void clear_reg_C(uint32_t* reg_C) {
     }
 }
 
-__global__ void my_conv_kernel(int M, int c_in, int N, int kernel_volume, half *__restrict__ A, 
-                    half *__restrict__ B, int *__restrict__ out_in_map, half *__restrict__ C) {
+__global__ void my_conv_kernel(int M, int c_in, int N, int kernel_volume, 
+                               int* reorder_map, int* loc_map, int* valid_map,
+                               half *__restrict__ A, half *__restrict__ B, half *__restrict__ C) {
     __shared__ half shm_A[64 * 64];
     __shared__ half shm_B[64 * 64];
     __shared__ float shm_C[64 * 64];
@@ -126,7 +129,9 @@ __global__ void my_conv_kernel(int M, int c_in, int N, int kernel_volume, half *
     int K = c_in * kernel_volume;
 
     for (int k = 0; k < K / 64; k++) {
-        load_shm_A(shm_A, A, out_in_map, c_in, kernel_volume, k);
+        int offset = k * 64 / c_in * M + blockIdx.x * 64;
+        int valid = valid_map[k * 64 / c_in * M / 64 + blockIdx.x];
+        load_shm_A(shm_A, A, reorder_map + offset, valid, M, c_in, k);
         load_shm_B(shm_B, B, N, K, k);
         __syncthreads();
 
@@ -134,7 +139,7 @@ __global__ void my_conv_kernel(int M, int c_in, int N, int kernel_volume, half *
             load_reg_B(reg_B, shm_B, ki);
         }
 
-        for (int m = 0; m < 4; m++) {
+        for (int m = 0; m < valid; m++) {
             clear_reg_C(reg_C);
             for (int ki = 0; ki < 4; ki++) {
                 load_reg_A(reg_A, shm_A, m, ki);
@@ -148,25 +153,27 @@ __global__ void my_conv_kernel(int M, int c_in, int N, int kernel_volume, half *
                 }
             }
             __syncthreads();
-            store_reg_C(reg_C, shm_C, m);
+            store_reg_C(reg_C, shm_C, loc_map + offset, m);
         }
     }
     store_shm_C(shm_C, C, M, N);
 }
 
-at::Tensor my_conv_cuda(torch::Tensor _in_feats, torch::Tensor _kernel, torch::Tensor _out_in_map, 
-                        int n_points, int c_out) 
+at::Tensor my_conv_cuda(torch::Tensor _in_feats, torch::Tensor _kernel, torch::Tensor _reorder_map,
+                        torch::Tensor _loc_map, torch::Tensor _valid_map, int n_points, int c_out) 
 {
     int num_in_feats = _in_feats.size(0);
     int c_in = _in_feats.size(1);
-    int kernel_volume = _out_in_map.size(1);
+    int kernel_volume = _reorder_map.size(0);
 
     assert(kernel_volume >= 4);
     
     auto options = torch::TensorOptions().dtype(_in_feats.dtype()).device(_in_feats.device());
     at::Tensor _out_feats = torch::empty({n_points, c_out}, options);
 
-    int* out_in_map = _out_in_map.data_ptr<int>();
+    int* reorder_map = _reorder_map.data_ptr<int>();
+    int* loc_map = _loc_map.data_ptr<int>();
+    int* valid_map = _valid_map.data_ptr<int>();
     half* in_feats = reinterpret_cast<half *>(_in_feats.data_ptr<at::Half>());
     half* kernel = reinterpret_cast<half *>(_kernel.data_ptr<at::Half>());
     half* out_feats = reinterpret_cast<half *>(_out_feats.data_ptr<at::Half>());
@@ -175,11 +182,11 @@ at::Tensor my_conv_cuda(torch::Tensor _in_feats, torch::Tensor _kernel, torch::T
     // printf("%d %d\n", c_in, c_out);
     // assert(c_in % 32 == 0);
 
-    int M = _out_feats.size(0);
+    int M = n_points;
     int N = c_out;
 
     my_conv_kernel<<<dim3(cdiv(M, 64), cdiv(N, 64)), dim3(32, 2)>>>
-                    (M, c_in, c_out, kernel_volume, in_feats, kernel, out_in_map, out_feats);
+                    (M, c_in, c_out, kernel_volume, reorder_map, loc_map, valid_map, in_feats, kernel, out_feats);
     
     return _out_feats;
 }
--- torchsparse/backend/convolution/my_conv_cuda.h
+++ torchsparse/backend/convolution/my_conv_cuda.h
@@ -1,4 +1,4 @@
 #include <torch/extension.h>
 
-at::Tensor my_conv_cuda(torch::Tensor _in_feats, torch::Tensor _kernel,
-                       torch::Tensor _out_in_map, int num_out_feats, int num_out_channels);
+at::Tensor my_conv_cuda(torch::Tensor _in_feats, torch::Tensor _kernel, torch::Tensor _reorder_map,
+                        torch::Tensor _loc_map, torch::Tensor _valid_map, int n_points, int c_out);
