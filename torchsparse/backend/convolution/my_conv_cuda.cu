1a83bd257387340a57ea04f1a657e87b3a9ff55b
wangxuezhu <2565439576@qq.com>
2024-03-22 13:33:51 +0000

    update my_conv

--- torchsparse/backend/convolution/my_conv_cuda.cu
+++ torchsparse/backend/convolution/my_conv_cuda.cu
@@ -12,7 +12,7 @@
 
 #define cdiv(x, y) (((x) + (y) - 1) / (y))
 
-__device__ void load_shm_A(half *shm_A, half *A, int* out_in_map, int num_in_channels, int kernel_volume, int ko)
+__device__ void load_shm_A(half *shm_A, half *A, int* out_in_map, int c_in, int kernel_volume, int ko)
 {
     // layout: [64, 64]
     int tid = threadIdx.y * 32 + threadIdx.x;
@@ -21,13 +21,13 @@ __device__ void load_shm_A(half *shm_A, half *A, int* out_in_map, int num_in_cha
         int col = tid % 8 * 8;
         int shm_row = row;
         int shm_col = col ^ ((shm_row & 3) << 3);
-        int row_A = out_in_map[(blockIdx.x * 64 + row) * kernel_volume + (ko * 64 + col) / num_in_channels];
-        int col_A = (ko * 64 + col) % num_in_channels;
+        int row_A = out_in_map[(blockIdx.x * 64 + row) * kernel_volume + (ko * 64 + col) / c_in];
+        int col_A = (ko * 64 + col) % c_in;
         void* ptr = &shm_A[shm_row * 64 + shm_col];
         if (row_A == -1) {
             *(float4*)ptr = make_float4(0, 0, 0, 0);
         } else {
-            *(float4*)ptr = *(float4*)&A[row_A * num_in_channels + col_A];
+            *(float4*)ptr = *(float4*)&A[row_A * c_in + col_A];
         }
     }
     __syncthreads();
@@ -112,7 +112,7 @@ __device__ void clear_reg_C(uint32_t* reg_C) {
     }
 }
 
-__global__ void my_conv_kernel(int M, int num_in_channels, int N, int kernel_volume, half *__restrict__ A, 
+__global__ void my_conv_kernel(int M, int c_in, int N, int kernel_volume, half *__restrict__ A, 
                     half *__restrict__ B, int *__restrict__ out_in_map, half *__restrict__ C) {
     __shared__ half shm_A[64 * 64];
     __shared__ half shm_B[64 * 64];
@@ -123,10 +123,10 @@ __global__ void my_conv_kernel(int M, int num_in_channels, int N, int kernel_vol
     uint32_t reg_C[4 * 4];
     clear_shm_C(shm_C);
     
-    int K = num_in_channels * kernel_volume;
+    int K = c_in * kernel_volume;
 
     for (int k = 0; k < K / 64; k++) {
-        load_shm_A(shm_A, A, out_in_map, num_in_channels, kernel_volume, k);
+        load_shm_A(shm_A, A, out_in_map, c_in, kernel_volume, k);
         load_shm_B(shm_B, B, N, K, k);
         __syncthreads();
 
@@ -155,36 +155,31 @@ __global__ void my_conv_kernel(int M, int num_in_channels, int N, int kernel_vol
 }
 
 at::Tensor my_conv_cuda(torch::Tensor _in_feats, torch::Tensor _kernel, torch::Tensor _out_in_map, 
-                        int num_out_feats, int num_out_channels) 
+                        int n_points, int c_out) 
 {
     int num_in_feats = _in_feats.size(0);
-    int num_in_channels = _in_feats.size(1);
+    int c_in = _in_feats.size(1);
     int kernel_volume = _out_in_map.size(1);
 
     assert(kernel_volume >= 4);
     
     auto options = torch::TensorOptions().dtype(_in_feats.dtype()).device(_in_feats.device());
-    at::Tensor _out_feats = torch::empty({num_out_feats, num_out_channels}, options);
+    at::Tensor _out_feats = torch::empty({n_points, c_out}, options);
 
     int* out_in_map = _out_in_map.data_ptr<int>();
     half* in_feats = reinterpret_cast<half *>(_in_feats.data_ptr<at::Half>());
     half* kernel = reinterpret_cast<half *>(_kernel.data_ptr<at::Half>());
     half* out_feats = reinterpret_cast<half *>(_out_feats.data_ptr<at::Half>());
 
-    // assert(num_out_channels % 64 == 0);
-    // printf("%d %d\n", num_in_channels, num_out_channels);
-    // assert(num_in_channels % 32 == 0);
+    // assert(c_out % 64 == 0);
+    // printf("%d %d\n", c_in, c_out);
+    // assert(c_in % 32 == 0);
 
     int M = _out_feats.size(0);
-    int N = num_out_channels;
-
-    // int smem_size = max(2 * (BLK_M + BLK_N) * BLK_K * 2, BLK_M * BLK_N * 4);
-    // if (smem_size >= (48 << 10)) {
-    //     cudaFuncSetAttribute(my_conv_kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, smem_size);
-    // }
+    int N = c_out;
 
     my_conv_kernel<<<dim3(cdiv(M, 64), cdiv(N, 64)), dim3(32, 2)>>>
-                    (M, num_in_channels, num_out_channels, kernel_volume, in_feats, kernel, out_in_map, out_feats);
+                    (M, c_in, c_out, kernel_volume, in_feats, kernel, out_in_map, out_feats);
     
     return _out_feats;
 }
