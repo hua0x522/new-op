f02eeb5a6077c6c291fcc23ffa1e1b1571e11950
wangxuezhu <2565439576@qq.com>
2024-04-13 06:46:41 +0000

    sparse_conv_cuda ldsm fast

--- torchsparse/backend/convolution/sparse_conv_cuda.cu
+++ torchsparse/backend/convolution/sparse_conv_cuda.cu
@@ -46,8 +46,8 @@ __device__ void load_shm_B_k64n64(half* shm_B, half* B, int K, int N, int ko) {
     __syncthreads();
 }
 
-__device__ void load_reg_A_k64n64(uint32_t* reg_A, half* shm_A, int ki) {
-    for (int m = 0; m < 8; m++) {
+__device__ void load_reg_A_k64n64(uint32_t* reg_A, half* shm_A, int ki, int m) {
+    // for (int m = 0; m < 8; m++) {
         int lane_id = threadIdx.x;
         int row = m * 16 + lane_id % 16;
         int col = ki * 16 + lane_id / 16 * 8;
@@ -55,7 +55,7 @@ __device__ void load_reg_A_k64n64(uint32_t* reg_A, half* shm_A, int ki) {
         int shm_col = col ^ ((shm_row & 7) << 3);
         uint32_t shm_A_lane_addr = __cvta_generic_to_shared(shm_A + shm_row * 64 + shm_col);
         LDMATRIX_X4(reg_A[ki * 32 + m * 4], reg_A[ki * 32 + m * 4 + 1], reg_A[ki * 32 + m * 4 + 2], reg_A[ki * 32 + m * 4 + 3], shm_A_lane_addr);
-    }
+    // }
 }
 
 __device__ void load_reg_B_k64n64(uint32_t* reg_B, half* shm_B, int ki) {
@@ -95,26 +95,31 @@ __device__ void pipe_load_k64n64(half* shm_A, half* shm_B, half* inputs, half* w
     load_shm_B_k64n64(shm_B, weights, kernel_size * c_in, N, ko);
 }
 
-__device__ void pipe_calc_k64n64(half* shm_A, half* shm_B, uint32_t* reg_A, uint32_t* reg_B, uint32_t* reg_C) {
+__device__ void pipe_calc_k64n64(half* shm_A, half* shm_B, uint32_t* reg_A, uint32_t* reg_B, uint32_t* reg_C, int mma_flag) {
     for (int ki = 0; ki < 4; ki++) {
-        load_reg_A_k64n64(reg_A, shm_A, ki);
+        // load_reg_A_k64n64(reg_A, shm_A, ki);
         load_reg_B_k64n64(reg_B, shm_B, ki);
     }
 
     for (int m = 0; m < 8; m++) {
-        for (int ki = 0; ki < 4; ki++) {
-            for (int n = 0; n < 2; n++) {
-                int idx = m * 2 + n;
-                HMMA16816(reg_C[idx * 4], reg_C[idx * 4 + 1], reg_C[idx * 4 + 2], reg_C[idx * 4 + 3],
-                    reg_A[ki * 32 + m * 4], reg_A[ki * 32 + m * 4 + 1], reg_A[ki * 32 + m * 4 + 2], reg_A[ki * 32 + m * 4 + 3],
-                    reg_B[ki * 4 + n * 2], reg_B[ki * 4 + n * 2 + 1],
-                    reg_C[idx * 4], reg_C[idx * 4 + 1], reg_C[idx * 4 + 2], reg_C[idx * 4 + 3]);
+        if (mma_flag & (1 << m)) {
+            for (int ki = 0; ki < 4l; ki++) {
+                load_reg_A_k64n64(reg_A, shm_A, ki, m);
+            }
+            for (int ki = 0; ki < 4; ki++) {
+                for (int n = 0; n < 2; n++) {
+                    int idx = m * 2 + n;
+                    HMMA16816(reg_C[idx * 4], reg_C[idx * 4 + 1], reg_C[idx * 4 + 2], reg_C[idx * 4 + 3],
+                        reg_A[ki * 32 + m * 4], reg_A[ki * 32 + m * 4 + 1], reg_A[ki * 32 + m * 4 + 2], reg_A[ki * 32 + m * 4 + 3],
+                        reg_B[ki * 4 + n * 2], reg_B[ki * 4 + n * 2 + 1],
+                        reg_C[idx * 4], reg_C[idx * 4 + 1], reg_C[idx * 4 + 2], reg_C[idx * 4 + 3]);
+                }
             }
         }
     }
 }
 
-__global__ void sparse_conv_k64n64(half* inputs, half* weights, int* reorder_map, int* reduced_mask, 
+__global__ void sparse_conv_k64n64(half* inputs, half* weights, int* reorder_map, int* reduced_mask, int* mma_mask,
                                        int* reorder_loc, half* outputs, 
                                        int n_points, int c_in, int c_out, int kernel_size) {
     int M = n_points;
@@ -130,18 +135,25 @@ __global__ void sparse_conv_k64n64(half* inputs, half* weights, int* reorder_map
     for (int ko = 0; ko < K / 64; ko++) {
         bool flag = reduced_mask[blockIdx.x] & (1 << (ko * 64 / c_in));
         if (flag) {
+            int mma_flag = 0;
+            for (int i = 0; i < 8; i++) {
+                if (mma_mask[blockIdx.x * 8 + i] & (1 << (ko * 64 / c_in))) {
+                    mma_flag = mma_flag + (1 << i); 
+                }
+            }
             pipe_load_k64n64(shm_A, shm_B, inputs, weights, reorder_map, kernel_size, c_in, N, ko);
             __pipeline_commit();
             __pipeline_wait_prior(0);
-            pipe_calc_k64n64(shm_A, shm_B, reg_A, reg_B, reg_C);
+            pipe_calc_k64n64(shm_A, shm_B, reg_A, reg_B, reg_C, mma_flag);
             __syncthreads();
         }
     }
     store_C_k64n64(reg_C, outputs, reorder_loc, M, N);
 }
 
-torch::Tensor sparse_conv_cuda(torch::Tensor inputs, torch::Tensor weights, torch::Tensor reorder_map,
-                              torch::Tensor reduced_mask, torch::Tensor reorder_loc, int num_out_feats) {
+torch::Tensor sparse_conv_cuda(torch::Tensor inputs, torch::Tensor weights, torch::Tensor reorder_map, 
+                               torch::Tensor reduced_mask, torch::Tensor mma_mask, torch::Tensor reorder_loc, 
+                               int num_out_feats) {
     int c_in = weights.size(1);
     int c_out = weights.size(2);
     int n_points = num_out_feats;
@@ -152,6 +164,7 @@ torch::Tensor sparse_conv_cuda(torch::Tensor inputs, torch::Tensor weights, torc
 
     int* reorder_map_ptr = reorder_map.data_ptr<int>();
     int* reduced_mask_ptr = reduced_mask.data_ptr<int>();
+    int* mma_mask_ptr = mma_mask.data_ptr<int>();
     int* reorder_loc_ptr = reorder_loc.data_ptr<int>();
     half* inputs_ptr = reinterpret_cast<half*>(inputs.data_ptr<at::Half>());
     half* weights_ptr = reinterpret_cast<half*>(weights.data_ptr<at::Half>());
@@ -160,7 +173,7 @@ torch::Tensor sparse_conv_cuda(torch::Tensor inputs, torch::Tensor weights, torc
     dim3 num_blocks(cdiv(n_points, 128), cdiv(c_out, 64));
     dim3 num_threads(32, 4);
     sparse_conv_k64n64<<<num_blocks, num_threads>>>
-                      (inputs_ptr, weights_ptr, reorder_map_ptr, reduced_mask_ptr, reorder_loc_ptr,
+                      (inputs_ptr, weights_ptr, reorder_map_ptr, reduced_mask_ptr, mma_mask_ptr, reorder_loc_ptr,
                       outputs_ptr, n_points, c_in, c_out, kernel_size);
     return outputs;
 }
--- torchsparse/backend/convolution/sparse_conv_cuda.h
+++ torchsparse/backend/convolution/sparse_conv_cuda.h
@@ -1,4 +1,5 @@
 #include <torch/extension.h>
 
 torch::Tensor sparse_conv_cuda(torch::Tensor inputs, torch::Tensor weights, torch::Tensor reorder_map,
-                              torch::Tensor reduced_mask, torch::Tensor reorder_loc, int num_out_feats);
+                              torch::Tensor reduced_mask, torch::Tensor mma_mask, torch::Tensor reorder_loc, 
+                              int num_out_feats);
